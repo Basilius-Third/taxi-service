6e0c0b98e1d42ef12cc6037fcd2826e27a3d6c07
Basilius the Third <champfockes@gmail.com>
2021-07-29 14:41:47 +0300

    added logger and toString methods to models

--- src/main/java/taxi/model/Manufacturer.java
+++ src/main/java/taxi/model/Manufacturer.java
@@ -59,4 +59,12 @@ public class Manufacturer {
     public int hashCode() {
         return Objects.hash(id, name, country);
     }
+
+    @Override
+    public String toString() {
+        return "Manufacturer{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", country='" + country + '\'' + '}';
+    }
 }
--- src/main/java/taxi/service/CarServiceImpl.java
+++ src/main/java/taxi/service/CarServiceImpl.java
@@ -1,6 +1,8 @@
 package taxi.service;
 
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import taxi.dao.CarDao;
 import taxi.lib.Inject;
 import taxi.lib.Service;
@@ -9,48 +11,57 @@ import taxi.model.Driver;
 
 @Service
 public class CarServiceImpl implements CarService {
+    private static final Logger logger = LogManager.getLogger(CarServiceImpl.class);
     @Inject
     private CarDao carDao;
 
     @Override
     public void addDriverToCar(Driver driver, Car car) {
+        logger.debug("Method addDriverToCar was called. " + driver + ". - " + car);
         car.getDrivers().add(driver);
         carDao.update(car);
     }
 
     @Override
     public void removeDriverFromCar(Driver driver, Car car) {
+        logger.debug("Method removeDriverFromCar was called. " + driver + ". - " + car);
         car.getDrivers().remove(driver);
         carDao.update(car);
     }
 
     @Override
     public List<Car> getAllByDriver(Long driverId) {
+        logger.debug("Method getAllByDriver was called. Driver id - " + driverId);
         return carDao.getAllByDriver(driverId);
     }
 
     @Override
     public Car create(Car car) {
+        logger.debug("Method create was called. " + car);
         return carDao.create(car);
     }
 
     @Override
     public Car get(Long id) {
+        logger.debug("Method get was called. Car id - " + id);
         return carDao.get(id).get();
     }
 
     @Override
     public List<Car> getAll() {
+        logger.debug("Method getAll was called.");
         return carDao.getAll();
     }
 
     @Override
     public Car update(Car car) {
+        logger.debug("Method update was called. " + car);
         return carDao.update(car);
     }
 
     @Override
     public boolean delete(Long id) {
+        logger.debug("Method delete was called. Car id - " + id);
         return carDao.delete(id);
     }
 }
